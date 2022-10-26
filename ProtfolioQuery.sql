d0743651979712cf289597195d54cdcb6e9c7b8c
DerekSip <85035190+DerekSip@users.noreply.github.com>
2022-10-26 11:12:24 -0400

    Add files via upload

---
+++ ProtfolioQuery.sql
@@ -0,0 +1,74 @@
+--Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths,
+--SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+--From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+--where continent is not null 
+--Group By date
+--order by 1,2
+
+Select *
+From PortfolioProject..CovidDeaths$
+Where continent is not null 
+order by 3,4
+
+-- Showing date off the imported exel sheets.
+Select location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths$
+order by 1,2
+
+-- Total cases Vs. Total deaths in the United States.
+-- And shows the likelihood of you dieing from covid if you get it.
+Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths$
+Where Location like '%states%'
+order by 1,2
+
+-- looking at total cases vs population
+-- shows percentage of population that got covid
+Select location, date, total_cases, Population, (total_deaths/population)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths$
+Where Location like '%states%'
+order by 1,2
+
+-- Countries with Highest Infection Rate compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- Countries with Highest Death Count per Population
+
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+--Worlds Covid numbers 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+
+
+
